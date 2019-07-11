7525f3e5dbe340aa92a07352d73925118e52b613
jdluong <luongjohnd@gmail.com>
2019-07-10 17:10:55 -0700

    added retry logic for lec and dis; using two boolean arrays

--- signMeUp.py
+++ signMeUp.py
@@ -2,13 +2,17 @@ from selenium import webdriver
 from selenium.webdriver.common.keys import Keys
 from bs4 import BeautifulSoup
 from selenium.common.exceptions import NoSuchElementException # retry login
+import re # regex
 import time # for testing
 
 
 deptName = 'BME'
 courseNum = '160' 
 lectureCode = ['13570','13515']
+# 180A --- 13580: full
 disCodes = [['13571','13572'],['13516,13518']]
+# 180A --- 13582: full, 13583: open
+# 170 --- 13573: full
 
 username = 'jdluong'
 pw = 'jawnlu2v33'
@@ -96,6 +100,7 @@ if needToCheck:
 ##### SIGNING IN ######
 #######################
 
+
 driver.get("https://www.reg.uci.edu/registrar/soc/webreg.html") # go to webreg site
 
 # gets the access webreg element, and clicks it
@@ -145,42 +150,90 @@ while not loggedIn:
 ### MULTIPLE CLASSES ###
 ########################
 
-for lectureInd in range(len(lectureCode)):
+# some data structures to indicate that lec and dis are successfull
+lecEnrolled = [False] * len(lectureCode)
+disEnrolled = [False] * len(lectureCode)
 
-	#######################
-	##### ADD LECTURE #####
-	#######################
+# maybe a while loop saying while False in lecEnrolled or False in disEnrolled?
+# but how would we get it to try to sign up for lec/dis that is False? 
+# maybe it's not as clean to write another condition for a "retry", or going back
+# so maybe we should have a condition of "if True: skip, if False, do it all again" in the for loop?
+while False in lecEnrolled and False in disEnrolled:
 
-	# finds "add" radio button and clicks it
-	add_radio = driver.find_element_by_xpath("//input[@type='radio'][@id='add']")
-	add_radio.click()
+	for lectureInd in range(len(lectureCode)):
 
-	# finds the input box for the code, input the lecture code, and submits it
-	courseCode_input = driver.find_element_by_name("courseCode")
-	courseCode_input.send_keys(lectureCode[lectureInd])
+		#######################
+		##### ADD LECTURE #####
+		#######################
 
-	# find the send request button and press it
-	sendRequest_button = driver.find_element_by_xpath("//input[@type='submit'][@value='Send Request']")
-	sendRequest_button.click()
+		if lecEnrolled[lectureInd] == False: # if lecture not signed up yet
+			# finds "add" radio button and clicks it
+			add_radio = driver.find_element_by_xpath("//input[@type='radio'][@id='add']")
+			add_radio.click()
 
-	# should check if signing up for lecture is successful; if not, repeat
+			# finds the input box for the code, input the lecture code, and submits it
+			courseCode_input = driver.find_element_by_name("courseCode")
+			courseCode_input.send_keys(lectureCode[lectureInd])
 
-	####################
-	##### ADD DIS ###### this is obnoxious because look below
-	####################
+			# find the send request button and press it
+			sendRequest_button = driver.find_element_by_xpath("//input[@type='submit'][@value='Send Request']")
+			sendRequest_button.click()
 
-	for prio, disCode in enumerate(disCodes[lectureInd]): # for each discussion code, in order of priority as inputted...
-		if needToCheck:
-			# 1) do we need these following two if-statements if the point of this block
-			# is to go through the priority list in order and sign up for whichever's
-			# open first?
-			# 2) the primary reason i can think of to keep these is to ensure their codes
-			# are right? but would we even need the "OPEN" statement
-			# 3) but to make the code more concise (and without if-statements), it would
-			# make sense to forego these big if-statements for the needToCheck = False case,
-			# because we wouldn't have a coursesDict
-			if disCode in coursesDict['Dis']:
-				if coursesDict['Dis'][disCode] == "OPEN": # if that discussion is open...
+			# check if lecture sign up was successful
+			time.sleep(1.5) # in case the next page takes a whiel to load
+			checkLecSoup = BeautifulSoup(driver.page_source,'html.parser')
+
+			if len(checkLecSoup.find_all(string=re.compile("You must successfully enroll in all co-classes"))) == 1: # if added before dis
+				print("Lecture",lectureCode[lectureInd],"signed up before discussion")
+				lecEnrolled[lectureInd] = True
+			elif len(checkLecSoup.find_all(string=re.compile("you have added"))) == 1: # if added after dis
+				print("Lecture",lectureCode[lectureInd],"signed up after discussion")
+				lecEnrolled[lectureInd] = True
+			elif len(checkLecSoup.find_all(string=re.compile("This course is full"))) == 1: # if unsuccessful
+				print("Lecture",lectureCode[lectureInd],"is full. Will try again later")
+				pass
+			
+			# should check if signing up for lecture is successful; if not, repeat
+
+		####################
+		##### ADD DIS ###### this is obnoxious because look below
+		####################
+		if disEnrolled[lectureInd] == False:
+			for prio, disCode in enumerate(disCodes[lectureInd]): # for each discussion code, in order of priority as inputted...
+				if needToCheck:
+					# 1) do we need these following two if-statements if the point of this block
+					# is to go through the priority list in order and sign up for whichever's
+					# open first?
+					# 2) the primary reason i can think of to keep these is to ensure their codes
+					# are right? but would we even need the "OPEN" statement
+					# 3) but to make the code more concise (and without if-statements), it would
+					# make sense to forego these big if-statements for the needToCheck = False case,
+					# because we wouldn't have a coursesDict
+					if disCode in coursesDict['Dis']:
+						if coursesDict['Dis'][disCode] == "OPEN": # if that discussion is open...
+							# find add radio button and click it
+							add_radio = driver.find_element_by_xpath("//input[@type='radio'][@id='add']")
+							add_radio.click()
+
+							# find course code input box and enter current discussion code
+							courseCode_input = driver.find_element_by_name("courseCode")
+							courseCode_input.send_keys(disCode)
+
+							# find send request button and click it
+							sendRequest_button = driver.find_element_by_xpath("//input[@type='submit'][@value='Send Request']")
+							sendRequest_button.click()
+
+							# have to check if it was successfully added
+							checkSoup = BeautifulSoup(driver.page_source,'html.parser')
+							addedCheck = checkSoup.find_all("h2")
+							if addedCheck[0].string.strip() == "you have added": # if successfully added AFTER lecture...
+								print('you got your priority #', prio+1, 'for lecture', lectureCode[lectureInd])
+								disEnrolled[lectureInd] == True
+								break # stop trying to add future discussions
+							elif 
+						else: # else, go to next discussion code 
+							pass
+				else:
 					# find add radio button and click it
 					add_radio = driver.find_element_by_xpath("//input[@type='radio'][@id='add']")
 					add_radio.click()
@@ -196,34 +249,21 @@ for lectureInd in range(len(lectureCode)):
 					# have to check if it was successfully added
 					checkSoup = BeautifulSoup(driver.page_source,'html.parser')
 					addedCheck = checkSoup.find_all("h2")
-					if addedCheck[0].string.strip() == "you have added": # if successfully added...
+
+
+					if len(checkLecSoup.find_all(string=re.compile("you have added"))) == 1: # if added after dis
+						lecEnrolled[lectureInd] = True
+						print("Discussion",disCode,"signed up after lecture")
 						print('you got your priority #', prio+1, 'for lecture', lectureCode[lectureInd])
-						break # stop trying to add future discussions
-				else: # else, go to next discussion code 
-					pass
-		else:
-			# find add radio button and click it
-			add_radio = driver.find_element_by_xpath("//input[@type='radio'][@id='add']")
-			add_radio.click()
-
-			# find course code input box and enter current discussion code
-			courseCode_input = driver.find_element_by_name("courseCode")
-			courseCode_input.send_keys(disCode)
-
-			# find send request button and click it
-			sendRequest_button = driver.find_element_by_xpath("//input[@type='submit'][@value='Send Request']")
-			sendRequest_button.click()
-
-			# have to check if it was successfully added
-			checkSoup = BeautifulSoup(driver.page_source,'html.parser')
-			addedCheck = checkSoup.find_all("h2")
-
-			if addedCheck[0].string.strip() == "you have added": # if successfully added...
-				print('you got your priority #', prio+1, 'for lecture', lectureCode[lectureInd])
-				break # stop trying to add future discussions
-			else: # else, go to next discussion code 
-				pass
-	# should add if a discussion was signed up for; if not keep trying
+						break
+					elif len(checkLecSoup.find_all(string=re.compile("You must successfully enroll in all co-classes"))) == 1: # if added before dis
+						lecEnrolled[lectureInd] = True
+						print("Discussion",disCode,"signed up before lecture")
+						print('you got your priority #', prio+1, 'for lecture', lectureCode[lectureInd])
+						break
+					elif len(checkLecSoup.find_all(string=re.compile("This course is full"))) == 1: # if unsuccessful
+						print("Discussion",disCode,"is full. Will try again later")
+						pass
 
 
 # driver.quit()
