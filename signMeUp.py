3da03d7452c169e9ffbf255da112ce0dc15a6418
jdluong <luongjohnd@gmail.com>
2019-07-17 16:29:49 -0700

    added print statements and tested with several cases; see planner notes

--- signMeUp.py
+++ signMeUp.py
@@ -7,12 +7,16 @@ import time # for testing
 
 
 deptName = 'BME'
-courseNum = '160' 
+courseNum = '160'
 lectureCode = ['13570','13515']
+# 160 --- 13570: open
+# 60B --- 13515: open
 # 180A --- 13580: full
-disCodes = [['13571','13572'],['13516,13518']]
-# 180A --- 13582: full, 13583: open
-# 170 --- 13573: full
+disCodes = [['13573','13572'],['13518','13516']]
+# 160 --- 13573: full; 13571: open; 13572: open
+# 60B --- 13518: time conflict; 13516: open
+# 180A --- 13582: full; 13583: open
+classNames = ['BME 160','BME 60B']
 
 username = 'jdluong'
 pw = 'jawnlu2v33'
@@ -111,6 +115,7 @@ access_webreg.click()
 #######################
 #### RETRY LOG IN #####
 #######################
+
 loggedIn = False
 while not loggedIn:
 	# i wanna use a try-except block because the other way would be to read in the entire
@@ -136,10 +141,10 @@ while not loggedIn:
 		enrollment_menu = driver.find_element_by_xpath("//input[@class='WebRegButton'][@value='Enrollment Menu']")
 		enrollment_menu.click()
 		loggedIn = True
-		print('logged in')
+		print('\nSuccessfully logged in')
 	# if login was unsuccessful...
 	except NoSuchElementException as exception:
-		print("Can't log in. Retrying in one minute.")
+		print("Can't log in. Retrying in one minute...")
 		time.sleep(60)
 		access_webreg = driver.find_element_by_xpath("//input[@type='submit'][@name='button'][@value='Access WebReg']")
 		access_webreg.click()
@@ -153,14 +158,37 @@ while not loggedIn:
 # some data structures to indicate that lec and dis are successfull
 lecEnrolled = [False] * len(lectureCode)
 disEnrolled = [False] * len(lectureCode)
-
+tries = 0
 # maybe a while loop saying while False in lecEnrolled or False in disEnrolled?
 # but how would we get it to try to sign up for lec/dis that is False? 
 # maybe it's not as clean to write another condition for a "retry", or going back
 # so maybe we should have a condition of "if True: skip, if False, do it all again" in the for loop?
-while False in lecEnrolled and False in disEnrolled:
+while False in lecEnrolled or False in disEnrolled:
+
+	# testing
+
+	tries += 1
+	print("\n----------------------------------------------------------\n")
+	print("***************")
+	print("*** TRY #",tries,"***")
+	print("***************")
+	# for i in range(len(lecEnrolled)):
+	# 	if lecEnrolled[i] and disEnrolled[i]:
+	# 		print("Successfully enrolled for LEC and DIS for", classNames[i])
+	# 	else:
+	# 		print("Still need to sign up for",classNames[i])
+	# 		if not lecEnrolled[i]:
+	# 			print('- need LEC')
+	# 		if not disEnrolled[i]:
+	# 			print('- need DIS')
+
+	# testing
 
 	for lectureInd in range(len(lectureCode)):
+
+		print('\n---------')
+		print(classNames[lectureInd],' |')
+		print('---------')
 
 		#######################
 		##### ADD LECTURE #####
@@ -180,20 +208,20 @@ while False in lecEnrolled and False in disEnrolled:
 			sendRequest_button.click()
 
 			# check if lecture sign up was successful
-			time.sleep(1.5) # in case the next page takes a whiel to load
+			time.sleep(1) # in case the next page takes a whiel to load
 			checkLecSoup = BeautifulSoup(driver.page_source,'html.parser')
 
 			if len(checkLecSoup.find_all(string=re.compile("You must successfully enroll in all co-classes"))) == 1: # if added before dis
-				print("Lecture",lectureCode[lectureInd],"signed up before discussion")
+				print("[x] LEC SUCCESS; signed up before discussion")
 				lecEnrolled[lectureInd] = True
 			elif len(checkLecSoup.find_all(string=re.compile("you have added"))) == 1: # if added after dis
-				print("Lecture",lectureCode[lectureInd],"signed up after discussion")
+				print("[x] LEC SUCCESS; signed up after discussion")
 				lecEnrolled[lectureInd] = True
 			elif len(checkLecSoup.find_all(string=re.compile("This course is full"))) == 1: # if unsuccessful
-				print("Lecture",lectureCode[lectureInd],"is full. Will try again later")
+				print("[ ] LEC IS FULL; will try again later")
 				pass
-			
-			# should check if signing up for lecture is successful; if not, repeat
+		
+		print('--')
 
 		####################
 		##### ADD DIS ###### this is obnoxious because look below
@@ -230,7 +258,6 @@ while False in lecEnrolled and False in disEnrolled:
 								print('you got your priority #', prio+1, 'for lecture', lectureCode[lectureInd])
 								disEnrolled[lectureInd] == True
 								break # stop trying to add future discussions
-							elif 
 						else: # else, go to next discussion code 
 							pass
 				else:
@@ -247,25 +274,45 @@ while False in lecEnrolled and False in disEnrolled:
 					sendRequest_button.click()
 
 					# have to check if it was successfully added
-					checkSoup = BeautifulSoup(driver.page_source,'html.parser')
-					addedCheck = checkSoup.find_all("h2")
+					# checkSoup = BeautifulSoup(driver.page_source,'html.parser')
+					# addedCheck = checkSoup.find_all("h2")
 
+					# checking if discussion sign up was successful
+					# time.sleep(1)
+					checkDisSoup = BeautifulSoup(driver.page_source,'html.parser')
 
-					if len(checkLecSoup.find_all(string=re.compile("you have added"))) == 1: # if added after dis
-						lecEnrolled[lectureInd] = True
-						print("Discussion",disCode,"signed up after lecture")
-						print('you got your priority #', prio+1, 'for lecture', lectureCode[lectureInd])
+					if len(checkDisSoup.find_all(string=re.compile("you have added"))) == 1: # if added after dis
+						disEnrolled[lectureInd] = True
+						print("[x] DIS",prio+1,"SUCCESS; signed up after lecture")
+						# print('    - you got your priority #', prio+1, 'for', classNames[lectureInd])
 						break
-					elif len(checkLecSoup.find_all(string=re.compile("You must successfully enroll in all co-classes"))) == 1: # if added before dis
-						lecEnrolled[lectureInd] = True
-						print("Discussion",disCode,"signed up before lecture")
-						print('you got your priority #', prio+1, 'for lecture', lectureCode[lectureInd])
+					elif len(checkDisSoup.find_all(string=re.compile("You must successfully enroll in all co-classes"))) == 1: # if added before dis
+						disEnrolled[lectureInd] = True
+						print("[x] DIS",prio+1,"SUCCESS; signed up before lecture")
+						# print('    - you got your priority #', prio+1, 'for', classNames[lectureInd])
 						break
-					elif len(checkLecSoup.find_all(string=re.compile("This course is full"))) == 1: # if unsuccessful
-						print("Discussion",disCode,"is full. Will try again later")
-						pass
+					elif len(checkDisSoup.find_all(string=re.compile("This course is full"))) == 1: # if unsuccessful
+						print("[ ] DIS",prio+1,"IS FULL; continue to try")
+					else:
+						print("[ ] DIS",prio+1,"ERROR")
 
+	# testing
+	print('\n**************')
+	print('*** STATUS ***')
+	print('**************\n')
+	for bothSec in range(len(classNames)):
+		if lecEnrolled[bothSec] and disEnrolled[bothSec]:
+			print('[x]',classNames[bothSec],'successfully enrolled')
+		elif not lecEnrolled[bothSec] or not disEnrolled[bothSec]:
+			print('[ ]',classNames[bothSec],'NOT enrolled')
+			if not lecEnrolled[bothSec]:
+				print('- need LEC')
+			if not disEnrolled[bothSec]:
+				print('- need DIS')
 
+	# if tries == 3:
+	# 	break
+	# testing
 # driver.quit()
 
 ###			    ###
