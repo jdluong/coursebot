1810473d9c953f7558c1e8d33350dcc1b6c8f432
jdluong <luongjohnd@gmail.com>
2019-09-11 15:02:08 -0700

    enroll dis and lec classes tested successfully

--- signMeUp.py
+++ signMeUp.py
@@ -17,7 +17,7 @@ import time # for testing
 
 def checkStatus(deptName,courseNum,prioLec,waitTimeScrape):
 	"""
-	Creates scraper class to scrape for course statuses. Sends email notif if it's open
+	Creates scraper class to scrape for course statuses and sends email notif if it's open
 
 	type deptName, courseNum, prioLec: string
 	
@@ -28,11 +28,9 @@ def checkStatus(deptName,courseNum,prioLec,waitTimeScrape):
 	could make it checkStatus(scraperObj,waitTimeScrape) instead
 	"""
 	scraper = Scraper(deptName,courseNum)
-	OPEN = False # used to keep refreshing and scraping until it's open
-
+	OPEN = False 
 	while not OPEN:
 		coursesDict = scraper.run_scrape()
-		# after getting statuses of all courses, check if lecture is open
 		if coursesDict['Lec'][prioLec] == 'OPEN':
 			print(deptName,courseNum,"is open! Sending email notification...")
 			OPEN = True
@@ -44,244 +42,26 @@ def checkStatus(deptName,courseNum,prioLec,waitTimeScrape):
 	
 	return coursesDict
 
-def login():
-	""" should break this down into logging in and signing up """
-
-	username = 'jdluong'
-	pw = 'jawnlu2v33'
-
-	driver = webdriver.Chrome() # open chrome window
-
-	driver.get("https://www.reg.uci.edu/registrar/soc/webreg.html") # go to webreg site
-
-	# gets the access webreg element, and clicks it
-	access_webreg = driver.find_element_by_xpath("//a[@href='https://www.reg.uci.edu/cgi-bin/webreg-redirect.sh']")
-	access_webreg.click()
-
-
-	#######################
-	#### RETRY LOG IN #####
-	#######################
-
-	loggedIn = False
-	while not loggedIn:
-		# i wanna use a try-except block because the other way would be to read in the entire
-		# web page each time to check for the existence of the enrollment_menu window w/ BS, 
-		# but i feel like that'd take more time to do than just to keep trying to check for it
-		# w/ selenium, which throws an error, and then the error is handled. but it seems like
-		# that sounds like bad practice/an anti-pattern
-		try:
-			# enter ucinetid and pw 
-			ucinetid = driver.find_element_by_name("ucinetid")
-			ucinetid.send_keys(username)
-			password = driver.find_element_by_name("password")
-			password.send_keys(pw)
-			password.send_keys('\ue007')
-
-			# # for testing
-			# logout_button = driver.find_element_by_xpath("//input[@value='Logout'][@type='submit']")
-			# logout_button.click()
-			# # for testing
-
-			# if enrollment_menu can't be found, then that means login wasn't successful
-			# exception is then handled in the except block
-			enrollment_menu = driver.find_element_by_xpath("//input[@class='WebRegButton'][@value='Enrollment Menu']")
-			enrollment_menu.click()
-			loggedIn = True
-			print('\nSuccessfully logged in')
-		# if login was unsuccessful...
-		except NoSuchElementException as exception:
-			print("Can't log in. Retrying in one minute...")
-			time.sleep(60)
-			access_webreg = driver.find_element_by_xpath("//input[@type='submit'][@name='button'][@value='Access WebReg']")
-			access_webreg.click()
-
-	# ONCE SUCCESSFULLY LOGGED IN...
-
 def enrollment(lectureCodes,disCodes,classNames):
-
-	lectureCodes = ['13570','13515']
-	# 160 --- 13570: open
-	# 60B --- 13515: open
-	# 180A --- 13580: full
-	disCodes = [['13573','13572'],['13518','13516']]
-	# 160 --- 13573: full; 13571: open; 13572: open
-	# 60B --- 13518: time conflict; 13516: open
-	# 180A --- 13582: full; 13583: open
-	classNames = ['BME 160','BME 60B']
-
-	username = 'jdluong'
-	pw = 'jawnlu2v33'
-
-	########################
-	### MULTIPLE CLASSES ###
-	########################
-
-	# some data structures to indicate that lec and dis are successfull
-	lecEnrolled = [False] * len(lectureCodes)
-	disEnrolled = [False] * len(lectureCodes)
-	tries = 0
-	# maybe a while loop saying while False in lecEnrolled or False in disEnrolled?
-	# but how would we get it to try to sign up for lec/dis that is False? 
-	# maybe it's not as clean to write another condition for a "retry", or going back
-	# so maybe we should have a condition of "if True: skip, if False, do it all again" in the for loop?
-	while False in lecEnrolled or False in disEnrolled:
-
-		# testing
-
-		tries += 1
-		print("\n---------------------------------------------------------------------------\n")
-		print("***************")
-		print("*** TRY #",tries,"***")
-		print("***************")
-
-		for lectureInd in range(len(lectureCodes)):
-
-			print('\n---------')
-			print(classNames[lectureInd],'|')
-			print('---------')
-
-			#######################
-			##### ADD LECTURE #####
-			#######################
-
-			if lecEnrolled[lectureInd] == False: # if lecture not signed up yet
-				# finds "add" radio button and clicks it
-				add_radio = driver.find_element_by_xpath("//input[@type='radio'][@id='add']")
-				add_radio.click()
-
-				# finds the input box for the code, input the lecture code, and submits it
-				courseCode_input = driver.find_element_by_name("courseCode")
-				courseCode_input.send_keys(lectureCodes[lectureInd])
-
-				# find the send request button and press it
-				sendRequest_button = driver.find_element_by_xpath("//input[@type='submit'][@value='Send Request']")
-				sendRequest_button.click()
-
-				# check if lecture sign up was successful
-				time.sleep(1) # in case the next page takes a whiel to load
-				checkLecSoup = BeautifulSoup(driver.page_source,'html.parser')
-
-				if checkLecSoup.find_all(string=re.compile("You must successfully enroll in all co-classes")): # if added before dis
-					print("[x] LEC SUCCESS; signed up before discussion")
-					lecEnrolled[lectureInd] = True
-				elif checkLecSoup.find_all(string=re.compile("you have added")): # if added after dis
-					print("[x] LEC SUCCESS; signed up after discussion")
-					lecEnrolled[lectureInd] = True
-				elif checkLecSoup.find_all(string=re.compile("This course is full")): # if unsuccessful
-					print("[ ] LEC IS FULL; will try again later")
-					pass
-			
-			print('--')
-
-			####################
-			##### ADD DIS ###### this is obnoxious because look below
-			####################
-			if disEnrolled[lectureInd] == False:
-				for prio, disCode in enumerate(disCodes[lectureInd]): # for each discussion code, in order of priority as inputted...
-					if needToCheck:
-						# 1) do we need these following two if-statements if the point of this block
-						# is to go through the priority list in order and sign up for whichever's
-						# open first?
-						# 2) the primary reason i can think of to keep these is to ensure their codes
-						# are right? but would we even need the "OPEN" statement
-						# 3) but to make the code more concise (and without if-statements), it would
-						# make sense to forego these big if-statements for the needToCheck = False case,
-						# because we wouldn't have a coursesDict
-						if disCode in coursesDict['Dis']:
-							if coursesDict['Dis'][disCode] == "OPEN": # if that discussion is open...
-								# find add radio button and click it
-								add_radio = driver.find_element_by_xpath("//input[@type='radio'][@id='add']")
-								add_radio.click()
-
-								# find course code input box and enter current discussion code
-								courseCode_input = driver.find_element_by_name("courseCode")
-								courseCode_input.send_keys(disCode)
-
-								# find send request button and click it
-								sendRequest_button = driver.find_element_by_xpath("//input[@type='submit'][@value='Send Request']")
-								sendRequest_button.click()
-
-								# have to check if it was successfully added
-								checkSoup = BeautifulSoup(driver.page_source,'html.parser')
-								addedCheck = checkSoup.find_all("h2")
-								if addedCheck[0].string.strip() == "you have added": # if successfully added AFTER lecture...
-									print('you got your priority #', prio+1, 'for lecture', lectureCodes[lectureInd])
-									disEnrolled[lectureInd] == True
-									break # stop trying to add future discussions
-							else: # else, go to next discussion code 
-								pass
-					else:
-						# find add radio button and click it
-						add_radio = driver.find_element_by_xpath("//input[@type='radio'][@id='add']")
-						add_radio.click()
-
-						# find course code input box and enter current discussion code
-						courseCode_input = driver.find_element_by_name("courseCode")
-						courseCode_input.send_keys(disCode)
-
-						# find send request button and click it
-						sendRequest_button = driver.find_element_by_xpath("//input[@type='submit'][@value='Send Request']")
-						sendRequest_button.click()
-
-						# have to check if it was successfully added
-						# checkSoup = BeautifulSoup(driver.page_source,'html.parser')
-						# addedCheck = checkSoup.find_all("h2")
-
-						# checking if discussion sign up was successful
-						# time.sleep(1)
-						checkDisSoup = BeautifulSoup(driver.page_source,'html.parser')
-
-						if len(checkDisSoup.find_all(string=re.compile("you have added"))) == 1: # if added after dis
-							disEnrolled[lectureInd] = True
-							print("[x] DIS",prio+1,"SUCCESS; signed up after lecture")
-							# print('    - you got your priority #', prio+1, 'for', classNames[lectureInd])
-							break
-						elif len(checkDisSoup.find_all(string=re.compile("You must successfully enroll in all co-classes"))) == 1: # if added before dis
-							disEnrolled[lectureInd] = True
-							print("[x] DIS",prio+1,"SUCCESS; signed up before lecture")
-							# print('    - you got your priority #', prio+1, 'for', classNames[lectureInd])
-							break
-						elif len(checkDisSoup.find_all(string=re.compile("This course is full"))) == 1: # if unsuccessful
-							print("[ ] DIS",prio+1,"IS FULL; continue to try")
-						else:
-							print("[ ] DIS",prio+1,"ERROR")
-
-		# testing
-		print('\n**************')
-		print('*** STATUS ***')
-		print('**************\n')
-		for bothSec in range(len(classNames)):
-			if lecEnrolled[bothSec] and disEnrolled[bothSec]:
-				print('[x]',classNames[bothSec],'successfully enrolled')
-			elif not lecEnrolled[bothSec] or not disEnrolled[bothSec]:
-				print('[ ]',classNames[bothSec],'NOT enrolled')
-				if not lecEnrolled[bothSec]:
-					print('- need LEC')
-				if not disEnrolled[bothSec]:
-					print('- need DIS')
-
-		# if tries == 3:
-		# 	break
-		# testing
-	# driver.quit()
-
-	###			    ###
-	### END PROGRAM ###
-	###		    	###
+	pass
 
 if __name__ == "__main__":
 	needToCheck = False
-
-	lectureCodes = ['13570','13515']
+	
+	lectureCodes = ['70230','70350','70460']
+	# lectureCodes = ['13570','13580','13515']
 	# 160 --- 13570: open
-	# 60B --- 13515: open
 	# 180A --- 13580: full
-	disCodes = [['13573','13572'],['13518','13516']]
+	# 60B --- 13515: open
+
+	disCodes = [[],[],[]]
+	# disCodes = [['13573','13571','13572'],['13582','13583'],['13518','13516']]
 	# 160 --- 13573: full; 13571: open; 13572: open
-	# 60B --- 13518: time conflict; 13516: open
 	# 180A --- 13582: full; 13583: open
-	classNames = ['BME 160','BME 60B']
+	# 60B --- 13518: time conflict; 13516: open
+	
+	classNames = ['SOCSCI 17','SOCSCI 115D','SOCSCI 163A']
+	#classNames = ['BME 160','BME 180A','BME 60B']
 
 	# ask for login info and confirm it's valid
 	session = SignUpper(lectureCodes,disCodes,classNames)
@@ -300,8 +80,10 @@ if __name__ == "__main__":
 		# yet how we're going to do the multiple checkStatuses
 		#
 		# SO FOR NOW, we'll have 4 parameters...
-		checkStatus(deptName,courseNum,prioLec,waitTimeScrape)
+		coursesDict = checkStatus(deptName,courseNum,prioLec,waitTimeScrape)
+	else: 
+		coursesDict = None
 
-	#session.login()
-	#session.enroll()
+	session.login(headless=True)
+	session.enroll(coursesDict)
 
--- signupper.py
+++ signupper.py
@@ -20,30 +20,55 @@ class SignUpper(LoginSetup):
         self.lectureCodes = lectureCodes
         self.disCodes = disCodes
         self.loginTimer = 60
-        
         self.lecEnrolled = [False] * len(self.lectureCodes)
         self.disEnrolled = [False] * len(self.lectureCodes)
 
-    def login(self):
+    def login(self,headless):
+        """
+        logging in while handling exceptions and checking status
+        """
         if not self.driver:
-            self.init_browser(False)
+            self.init_browser(headless)
         loggedIn = False
         while not loggedIn:
             try:
                 self.login_webauth(self.driver)
                 checkLoginSoup = BeautifulSoup(self.driver.page_source,'html.parser')
-                # for testing
                 # find_n_click_xpath(self.driver, elements.LOGOUT_BUTTON)
                 # checkLoginSoup = BeautifulSoup(self.driver.page_source,'html.parser')
-
-                # if enrollment_menu can't be found, then that means login wasn't successful
-                find_n_click_xpath(self.driver,elements.ENROLLMENT_MENU)
+                find_n_click_xpath(self.driver,elements.ENROLLMENT_MENU) # exception here if can't log in
                 loggedIn = True
-                print('Successfully logged in!')
+                print('Successfully logged into WebReg!\nBeginning enrollment process...')
             except NoSuchElementException: # if login was unsuccessful...
                 self.login_status(checkLoginSoup,self.WebRegExtension,self.loginTimer)
             except:
                 print("Something went very wrong")
+
+    def enroll(self,coursesDict):
+        """
+        method for enrollment process logic for lecture and discussion
+
+        type coursesDict: dict
+        """
+        tries = 0
+        while False in self.lecEnrolled or False in self.disEnrolled:
+            tries += 1 # for testing
+            print("\n///\n*** ATTEMPT #{tryNum} ***\n///".format(tryNum=tries))
+            for lectureInd in range(len(self.lectureCodes)):
+                if not self.lecEnrolled[lectureInd]:
+                    self.enroll_lec(lectureInd)               
+                if not self.disEnrolled[lectureInd]:
+                    # for each discussion code, in order of priority as inputted...
+                    for prio in range(len(self.disCodes[lectureInd])):
+                        added = self.enroll_dis(prio,lectureInd,coursesDict)
+                        if added: break
+                    print('------------')
+            self.print_courses_status(tries)
+        # start test code
+            if tries == 3: break
+        find_n_click_xpath(self.driver, elements.LOGOUT_BUTTON)
+        # end test code
+# ---------------------- for webregextension in self.login() ----------------------
 
     def WebRegExtension(self,checkLoginSoup):
         """
@@ -60,51 +85,7 @@ class SignUpper(LoginSetup):
             self.WebRegWait(self.loginTimer)
             find_n_click_xpath(self.driver,elements.ACCESS_WEBREG_webreg)
 
-    def enroll(self,coursesDict):
-        tries = 0
-        while False in self.lecEnrolled or False in self.disEnrolled:
-            tries += 1 # for testing
-            print("\n---------------------------------------------------------------------------\n")
-            print("***************")
-            print("*** TRY #",tries,"***")
-            print("***************")
-            for lectureInd in range(len(self.lectureCodes)):
-                if not self.lecEnrolled[lectureInd]:
-                    self.enroll_lec(lectureInd)               
-                if not self.disEnrolled[lectureInd]:
-                    # for each discussion code, in order of priority as inputted...
-                    for prio in range(len(self.disCodes[lectureInd])):
-                        added = self.enroll_dis(prio,lectureInd,coursesDict)
-                        if added: break
-
-            # testing
-            print('\n**************')
-            print('*** STATUS ***')
-            print('**************\n')
-            for bothSec in range(len(classNames)):
-                if lecEnrolled[bothSec] and disEnrolled[bothSec]:
-                    print('[x]',classNames[bothSec],'successfully enrolled')
-                elif not lecEnrolled[bothSec] or not disEnrolled[bothSec]:
-                    print('[ ]',classNames[bothSec],'NOT enrolled')
-                    if not lecEnrolled[bothSec]:
-                        print('- need LEC')
-                    if not disEnrolled[bothSec]:
-                        print('- need DIS')
-
-            # if tries == 3:
-            # 	break
-            # testing
-        # driver.quit()
-
-    def add_course(self,driver,courseCode):
-        """
-        in webreg, adds the course specified with the classCode
-
-        type lectureCode: string
-        """
-        find_n_click_xpath(driver,elements.ADD_RADIO)
-        find_n_sendkeys(driver,elements.INPUT_COURSECODE,courseCode)
-        find_n_click_xpath(driver,elements.SEND_REQUEST)
+# ---------------------- for lecture enrollment in self.enroll(coursesDict) ----------------------
 
     def enroll_lec(self,lectureInd):
         """
@@ -115,7 +96,7 @@ class SignUpper(LoginSetup):
         self.add_course(self.driver,self.lectureCodes[lectureInd])
         checkLecSoup = BeautifulSoup(self.driver.page_source,'html.parser')
         self.check_lec_status(checkLecSoup,lectureInd)
-        print('--')
+        print('-')
 
     def check_lec_status(self,checkLecSoup,lectureInd):
         """
@@ -136,6 +117,8 @@ class SignUpper(LoginSetup):
         elif checkLecSoup.find("div","WebRegErrorMsg"):
             print('[ ]', self.classNames[lectureInd], 'LEC UNABLE TO ENROLL: "{msg}"'.format(msg=checkLecSoup.find("div","WebRegErrorMsg").string.strip()))
             pass
+
+    # ---------------------- for discussion enrollment in self.enroll(coursesDict) ----------------------
 
     def enroll_dis(self,prio,lectureInd,coursesDict):
         """
@@ -162,30 +145,56 @@ class SignUpper(LoginSetup):
             return self.check_dis_status(checkDisSoup,prio,lectureInd)
 
     def check_dis_status(self,checkDisSoup,prio,lectureInd):
+        """
+        checks the status of the current discussion after attempting to enroll in it
+
+        type checkDisSoup: BeautifulSoup
+        type prio: int
+        type lectureInd: int
+        """
         if checkDisSoup.find_all(string=re.compile("you have added")): # if added after dis
             self.disEnrolled[lectureInd] = True
-            print("[x] {className} DIS SUCCESS (prio #{priority}); signed up after lecture".format(className=self.classNames[lectureInd],priority=prio+1))
+            print("[x] {className} DIS #{priority} SUCCESS; signed up after lecture".format(className=self.classNames[lectureInd],priority=prio+1))
             return True
         elif checkDisSoup.find_all(string=re.compile("You must successfully enroll in all co-classes")): # if added before dis
             self.disEnrolled[lectureInd] = True
-            print("[x] {className} DIS SUCCESS (prio #{priority}); signed up before lecture".format(className=self.classNames[lectureInd],priority=prio+1))
+            print("[x] {className} DIS #{priority} SUCCESS; signed up before lecture".format(className=self.classNames[lectureInd],priority=prio+1))
             return True
         elif checkDisSoup.find_all(string=re.compile("This course is full")) == 1: # if unsuccessful
-            print("[ ] DIS",prio+1,"IS FULL; will continue down prio")
+            print("[ ] {className} DIS #{priority} IS FULL; will continue down prio".format(className=self.classNames[lectureInd],priority=prio+1))
             return False
         elif checkDisSoup.find("div","WebRegErrorMsg"):
-            print('[ ]', self.classNames[lectureInd], 'DIS UNABLE TO ENROLL: "{msg}"'.format(msg=checkDisSoup.find("div","WebRegErrorMsg").string.strip()))
+            print('[ ] {className} DIS #{priority} UNABLE TO ENROLL: "{msg}"'.format(className=self.classNames[lectureInd],priority=prio+1,msg=checkDisSoup.find("div","WebRegErrorMsg").string.strip()))
             return False
-#----------------------------------- FANCY USER INPUT STUFF WILL DO LATER ------------------------------
-    
-    def courses_config(self):
-        # Class info
-        numClasses = int(input("Enter how many classes to sign up for: "))
-        for classNum in range(numClasses):
-            className = input("Name of course",classNum+1,"(e.g. ICS 31): ")
-            lecCode = input("Lecture code for {className}:".format(className=className))
-            separated = False
-            while not separated:
-                disCodesStr = input("Input {className}'s discussion codes, SEPARATED BY COMMAS: ".format(className=className))
-                # check if disCodes are right
-            # print: is this correct?
+
+# ---------------------- for printing status in self.enroll(coursesDict) ----------------------
+
+    def print_courses_status(self,tries):
+        """
+        prints course status after each try
+
+        type tries: int
+        """
+        # print('\n* STATUS #{tryNum} *'.format(tryNum=tries))
+        for bothSec in range(len(self.classNames)):
+            if self.lecEnrolled[bothSec] and self.disEnrolled[bothSec]:
+                print('[x] {name} successfully enrolled!'.format(name=self.classNames[bothSec]))
+            elif not self.lecEnrolled[bothSec] or not self.disEnrolled[bothSec]:
+                print('[ ] {name} NOT enrolled (need'.format(name=self.classNames[bothSec]),end='')
+                if not self.lecEnrolled[bothSec]:
+                    print(' LEC',end='')
+                if not self.disEnrolled[bothSec]:
+                    print(' DIS',end='')
+                print(')')
+
+    # ---------------------- all-purpose method for adding courses  ----------------------
+
+    def add_course(self,driver,courseCode):
+        """
+        in webreg, adds the course specified with the classCode
+
+        type lectureCode: string
+        """
+        find_n_click_xpath(driver,elements.ADD_RADIO)
+        find_n_sendkeys(driver,elements.INPUT_COURSECODE,courseCode)
+        find_n_click_xpath(driver,elements.SEND_REQUEST)
