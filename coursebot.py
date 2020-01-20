a6d6caf58b3693de386208e053e62304cf47fd89
jdluong <luongjohnd@gmail.com>
2020-01-19 17:45:14 -0800

    renamed run -> runners

--- coursebot.py
+++ coursebot.py
@@ -1,7 +1,7 @@
 import click
 from apscheduler.schedulers.blocking import BlockingScheduler
 
-import run
+import runners
 from tools import parse_datetime
 
 @click.group()
@@ -10,26 +10,26 @@ def main():
 
 @main.command()
 def scrape():
-    run.scrape()
+    runners.scrape()
     print("Exited successfully")
 
 @main.command()
 @click.option("--date","-d",default=None)
 @click.option("--time","-t",default=None)
 def enroll(date,time):
-    enroller = run.enroll_config()
+    enroller = runners.enroll_config()
     if date and time:
         datetime = parse_datetime(date,time)
         schedule = BlockingScheduler()
-        schedule.add_job(run.enroll_start, 'date', run_date=datetime, args=[enroller,schedule])
+        schedule.add_job(runners.enroll_start, 'date', run_date=datetime, args=[enroller,schedule])
         schedule.start()
     else:
-        run.enroll_start(enroller)
+        runners.enroll_start(enroller)
     print("Exited successfully")
 
 @main.command()
 def scrape_enroll():
-    run.scrape_enroll()
+    runners.scrape_enroll()
     print("Exited successfully")
 
 if __name__ == "__main__":
